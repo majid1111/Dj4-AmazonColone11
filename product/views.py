8b5d761a0e17ce73b47ca1e2682f3c9140eeab16
Majid Khamis <majid111111mm@gmail.com>
2023-09-06 11:17:26 +0400

    add product_list, product_detail ,urls

--- product/views.py
+++ product/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView ,DateDetailView
+from.models import Product , ProductImages , Brand , Review
+
+
 
 # Create your views here.
+
+class ProductList(ListView):
+    model = Product
+
+
+
+class ProductDetail(DateDetailView):
+    model = Product   
