49f2e88fde3a46cc3f2e026b17f88acc345299f0
Majid Khamis <majid111111mm@gmail.com>
2023-09-11 12:26:38 +0400

    add product slug

--- product/models.py
+++ product/models.py
@@ -1,9 +1,11 @@
+from collections.abc import Iterable
+from typing import Any
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from taggit.managers import TaggableManager
-
+from django.utils.text import slugify
 
 # Create your models here.
 FLAG_TYPES =(
@@ -24,12 +26,14 @@ class Product(models.Model):
     quantity = models.IntegerField(_('Quantity'))
     brand = models.ForeignKey('Brand',verbose_name=_('Brand'),related_name='product_brand',on_delete=models.SET_NULL,null=True)
     tags = TaggableManager()
+    slug = models.SlugField(null=True,blank=True)
     
     def __str__(self) :
         return self.name
 
-
-
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.name)
+        super(Product,self).save(*args,**kwargs)
 
 
 
